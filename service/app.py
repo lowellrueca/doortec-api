e8404fe9ceb3556698c48fd8f9cc92ebc9475cdb
lowellrueca <lowellrueca@gmail.com>
2022-06-15 05:50:03 +0800

    add on_startup and on_shutdown events

--- service/app.py
+++ service/app.py
@@ -1,10 +1,13 @@
 from starlette.applications import Starlette as App
 from starlette.routing import Mount
 
+
 def create_app():
     # imports
     from service.config import DEBUG
     from service.routes import schema, door
+    from service.events import on_startup, on_shutdown
+
 
     routes = [
         Mount(path="/schema", routes=schema.routes),
@@ -12,7 +15,12 @@ def create_app():
     ]
 
     # instatiate app
-    app = App(debug=DEBUG, routes=routes)
-    
+    app = App(
+        debug=DEBUG, 
+        routes=routes, 
+        on_startup=[on_startup], 
+        on_shutdown=[on_shutdown]
+    )
+
     # return app instance
     return app
