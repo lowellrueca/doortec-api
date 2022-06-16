a47bfcbd35d91acf738f89d4c33de33474faa3b6
lowellrueca <lowellrueca@gmail.com>
2022-06-16 21:48:26 +0800

    create a seed data function

--- service/db.py
+++ service/db.py
@@ -1,3 +1,4 @@
+import random
 from service import config
 from service.config import get_db_url
 
@@ -28,3 +29,13 @@ AERICH_CONFIG = {
     },
     "apps": {"models": {"models": ["service.models", "aerich.models"]}},
 }
+
+async def seed_data():
+    from service.models import Door
+    
+    models = await Door.all()
+    random_series = ["maine", "iowa", "prague", "florence", "lucerne", "zurich", "bruges", "stockholm", "nuremberg", "viena", "edinburgh", "venice", "amsterdam"]
+    random_prices = [2000.00, 2250.00, 3350.00, 4250.00, 5700.00, 7370.00, 8000.00, 9050.00]
+    mock_products = [Door(series=random.choice(random_series), price=random.choice(random_prices)) for i in range(300) if len(models) == 0]
+    await Door.bulk_create(mock_products)
+
