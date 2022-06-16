12caf9d49d12c3a9d0c34e78d1421630f348b103
lowellrueca <lowellrueca@gmail.com>
2022-06-16 21:49:15 +0800

    serving seed data funcion

--- service/events.py
+++ service/events.py
@@ -3,14 +3,11 @@ from service.db import DB_CONFIG
 
 
 async def on_startup():
+    from service.db import seed_data
+
     await Tortoise.init(config=DB_CONFIG)
     await Tortoise.generate_schemas()
-
-    # models: List[Door] = await Door.all()
-    # seed random data
-    # random_names = ["orange", "lemon", "banana", "grapes", "cherry", "avocado", "berry"]
-    # mock_products: List[Door] = [Product(name=random.choice(random_names)) for i in range(24) if len(models) == 0]
-    # await Door.bulk_create(mock_products)
+    await seed_data()
 
 
 async def on_shutdown():
